67ddcf87cb4d72e0fcea2cd1f910693eee03a6c7
exxsid <cortezleoanthony5@gmail.com>
2024-04-09 17:49:21 +0800

    Added Procedure view

---
+++ RecipeList/View/ProcedureView.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace RecipeList.View
+{
+    partial class ProcedureView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.procedurePhoto = new System.Windows.Forms.PictureBox();
+            this.description = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.Label();
+            this.category = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ingredients = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.procedures = new System.Windows.Forms.TextBox();
+            this.editBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.procedurePhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // procedurePhoto
+            // 
+            this.procedurePhoto.Location = new System.Drawing.Point(14, 14);
+            this.procedurePhoto.Name = "procedurePhoto";
+            this.procedurePhoto.Size = new System.Drawing.Size(214, 190);
+            this.procedurePhoto.TabIndex = 0;
+            this.procedurePhoto.TabStop = false;
+            // 
+            // description
+            // 
+            this.description.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.description.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.description.Enabled = false;
+            this.description.Font = new System.Drawing.Font("JetBrainsMono NF", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.description.Location = new System.Drawing.Point(245, 68);
+            this.description.Multiline = true;
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(286, 86);
+            this.description.TabIndex = 1;
+            this.description.Text = "Lorem ipsum dolor et met adhadf fjadfkjadfj alkdjf fajldkfj jladfkj jfjkdjfe fjad" +
+    " fajekaffjd jakdf";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("JetBrainsMono NF", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(247, 14);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(54, 21);
+            this.name.TabIndex = 2;
+            this.name.Text = "Name";
+            // 
+            // category
+            // 
+            this.category.AutoSize = true;
+            this.category.Font = new System.Drawing.Font("JetBrainsMono NF", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.category.Location = new System.Drawing.Point(248, 36);
+            this.category.Name = "category";
+            this.category.Size = new System.Drawing.Size(63, 14);
+            this.category.TabIndex = 3;
+            this.category.Text = "category";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("JetBrainsMono NF", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ingredients";
+            // 
+            // ingredients
+            // 
+            this.ingredients.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ingredients.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ingredients.Enabled = false;
+            this.ingredients.Font = new System.Drawing.Font("JetBrainsMono NF", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ingredients.Location = new System.Drawing.Point(28, 237);
+            this.ingredients.Multiline = true;
+            this.ingredients.Name = "ingredients";
+            this.ingredients.Size = new System.Drawing.Size(503, 154);
+            this.ingredients.TabIndex = 5;
+            this.ingredients.Text = "Lorem ipsum dolor et met adhadf fjadfkjadfj alkdjf fajldkfj jladfkj jfjkdjfe fjad" +
+    " fajekaffjd jakdf";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("JetBrainsMono NF", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 412);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Procedures";
+            // 
+            // procedures
+            // 
+            this.procedures.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.procedures.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.procedures.Enabled = false;
+            this.procedures.Font = new System.Drawing.Font("JetBrainsMono NF", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.procedures.Location = new System.Drawing.Point(28, 432);
+            this.procedures.Multiline = true;
+            this.procedures.Name = "procedures";
+            this.procedures.Size = new System.Drawing.Size(503, 211);
+            this.procedures.TabIndex = 7;
+            this.procedures.Text = "Lorem ipsum dolor et met adhadf fjadfkjadfj alkdjf fajldkfj jladfkj jfjkdjfe fjad" +
+    " fajekaffjd jakdf";
+            // 
+            // editBtn
+            // 
+            this.editBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(233)))), ((int)(((byte)(233)))));
+            this.editBtn.FlatAppearance.BorderSize = 0;
+            this.editBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.editBtn.Font = new System.Drawing.Font("JetBrainsMono NF", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editBtn.Location = new System.Drawing.Point(456, 690);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(75, 23);
+            this.editBtn.TabIndex = 8;
+            this.editBtn.Text = "Edit";
+            this.editBtn.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("JetBrainsMono NF", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(359, 690);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // ProcedureView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.editBtn);
+            this.Controls.Add(this.procedures);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ingredients);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.category);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.procedurePhoto);
+            this.Name = "ProcedureView";
+            this.Size = new System.Drawing.Size(549, 725);
+            ((System.ComponentModel.ISupportInitialize)(this.procedurePhoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox procedurePhoto;
+        private System.Windows.Forms.TextBox description;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label category;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ingredients;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox procedures;
+        private System.Windows.Forms.Button editBtn;
+        private System.Windows.Forms.Button button2;
+    }
+}
